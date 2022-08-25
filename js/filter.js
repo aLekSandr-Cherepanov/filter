8bf8e9e6af5feb364b988b49b7da99891884c559
Alexander Cherepanov <super.sany96@yandex.ru>
2022-08-25 16:41:53 +0300

    написание js кода

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,15 @@
 function app() {
     const button = document.querySelectorAll('.button')
+    const cards = document.querySelectorAll('.card')
+
+    function filter (category, items) {
+        items.forEach((item) => {
+            const isItemFiltered = !item.classList.contains(category)
+            if (isItemFiltered) {
+                item.classList.add('hide')
+            }
+        })
+    }
 
     button.forEach((button) => {
         button.addEventListener('click', () => {
