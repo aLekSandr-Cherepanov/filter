21f782c2f0ffc544de6db67120ccf95478201307
Alexander Cherepanov <super.sany96@yandex.ru>
2022-08-26 11:52:50 +0300

    закончил написание все работает

--- js/filter.js
+++ js/filter.js
@@ -5,8 +5,12 @@ function app() {
     function filter (category, items) {
         items.forEach((item) => {
             const isItemFiltered = !item.classList.contains(category)
-            if (isItemFiltered) {
-                item.classList.add('hide')
+            const isShowAll = category.toLowerCase() === 'all'
+            if (isItemFiltered && !isShowAll) {
+                item.classList.add('anime')
+            } else {
+                item.classList.remove('hide')
+                item.classList.remove('anime')
             }
         })
     }
@@ -17,6 +21,14 @@ function app() {
             filter(currentCategory, cards)
         })
     });
+
+    cards.forEach((card) => {
+        card.ontransitionend = function () {
+            if (card.classList.contains('anime')) {
+                card.classList.add('hide')
+            }
+        }
+    })
 }
 
 app()
